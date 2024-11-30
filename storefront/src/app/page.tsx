c2109e5fe7f6bed6f97526e4cebac4c4c27ebf01
webdevkaleem <webdevkaleem@gmail.com>
2024-11-30 18:13:43 +0500

    working api locally

--- storefront/src/app/page.tsx
+++ storefront/src/app/page.tsx
@@ -1,16 +1,7 @@
 export default async function Home() {
-  fetch(`http://localhost:9000/store/regions`, {
-    credentials: "include",
-    headers: {
-      "x-publishable-api-key": process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || "temp",
-    },
-  })
-  .then((res) => res.json())
-  .then(({ regions }) => {
-    // use regions...
-    console.log(regions)
-  })
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+    </>
   );
 }
